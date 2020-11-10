508d0d54cf718ae3e37c8754d5fe4d9c84202fdf
Sean Dyer <sean.dyer@jefferson.edu>
2020-11-10 16:01:40 -0500

    Add get hotfixes ps1 file.

---
+++ Get_PC_Hotfixes.ps1
@@ -0,0 +1,20 @@
+﻿<#---------------------------------
+    Author: Sean Dyer
+    Department: Educational Technologies
+    Description: This script will poll a remote PC for hotfix and security update information
+    Version: 1.0 (Initial version: 11/10/2020)
+-----------------------------------#>
+
+#Prompt the user for a computer name...
+$pcToConnectTo = Read-Host -Prompt "Which PC would you like to view Hotfix and Security Updates on?"
+
+#Let us determine if we've got a PC to work with...
+if ($pcToConnectTo) {
+    #Let us display some feedback to the user...
+    Write-Host "Getting Hotfixes..."
+    #Display hotfixes via stdout...
+    Get-HotFix -ComputerName $computer
+} else {
+    #Display a message to the user that they have to enter a PC name...
+    Write-Host "Please enter the PC's name..."
+}
