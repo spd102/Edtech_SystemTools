005500c62c053de5b5c41bcf753a9a4b23ec9789
Sean Dyer <sean.dyer@jefferson.edu>
2020-11-11 11:44:43 -0500

    Add ability to pipe output to  a text file to the current user's desktop. Add prompt for output file name.

--- Get_PC_Hotfixes.ps1
+++ Get_PC_Hotfixes.ps1
@@ -2,7 +2,7 @@
     Author: Sean Dyer
     Department: Educational Technologies
     Description: This script will poll a remote PC for hotfix and security update information
-    Version: 1.0 (Initial version: 11/10/2020)
+    Version: 1.0.1 (Initial version: 11/10/2020), version 1.0.1 (11/11/2020) added piping information to a text file
 -----------------------------------#>
 
 #Prompt the user for a computer name...
@@ -12,8 +12,18 @@ $pcToConnectTo = Read-Host -Prompt "Which PC would you like to view Hotfix and S
 if ($pcToConnectTo) {
     #Let us display some feedback to the user...
     Write-Host "Getting Hotfixes..."
-    #Display hotfixes via stdout...
-    Get-HotFix -ComputerName $computer
+    #Get the current user's desktop folder...
+    $currentUserDesktop = [environment]::GetFolderPath('Desktop')
+    #Prompt the user for the file name...
+    $fileName = Read-Host -Prompt "What would you like to call the output file?"
+    #Determine if we have a file name or not...
+    if ($fileName) {
+        #Run the get hotfix command and pipe it out to a text file...
+        Get-HotFix -ComputerName $pcToConnectTo | Out-File "$currentUserDesktop\$fileName.txt"
+    } else {
+        #Display output to the user...
+        Write-Host "Please enter a name for the output file..."
+    }
 } else {
     #Display a message to the user that they have to enter a PC name...
     Write-Host "Please enter the PC's name..."
