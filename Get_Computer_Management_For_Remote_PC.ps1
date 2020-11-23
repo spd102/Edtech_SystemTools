ec2b4d1e237eb6b5d2fae2920b87fffbe75e4dad
Sean Dyer <sean.dyer@jefferson.edu>
2020-11-23 16:01:08 -0500

    Add get computer management for remote computers ps1 file.

---
+++ Get_Computer_Management_For_Remote_PC.ps1
@@ -0,0 +1,17 @@
+﻿<#---------------------------------
+    Author: Sean Dyer
+    Department: Educational Technologies
+    Description: This script will open computer management on a remote PC.
+    Version: 1.0 (Initial version: 11/20/2020)
+    Usage: Get-RemoteComputerManagement -ComputerName "PCS122Q"
+-----------------------------------#>
+
+
+function Get-RemoteComputerManagement { [CmdletBinding()] 
+    param([Parameter(Mandatory)] [string] $ComputerName="PCS7996")
+    #Call the computer management snap-in and pass the parameter...
+    compmgmt /computer:$ComputerName
+}
+
+#Call the function
+Get-RemoteComputerManagement
