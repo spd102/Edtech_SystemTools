9619cf893282dea0fe57850eba2994107753c9d1
Sean Dyer <sean.dyer@jefferson.edu>
2021-02-20 16:05:25 -0500

    Add Get ChassisType ps1 file for determining the chassis type of a PC.

---
+++ Get_PC_ChassisType_v2.ps1
@@ -0,0 +1,24 @@
+﻿<#---------------------------------
+    Author: Sean Dyer
+    Department: Educational Technologies
+    Description: This script sets a custom registry property based on the PC's ChassisType
+    Version: 1.1 (Initial version: 11/5/2020)
+-----------------------------------#>
+
+# Determine which type of PC this is...
+$Chassis_Type_Number = (Get-WmiObject Win32_SystemEnclosure).ChassisTypes
+# Chassis Type values from https://technet.microsoft.com/en-us/library/ee156537.aspx
+
+#Filter off the number returned from the ChassisTypes collection
+Switch ($Chassis_Type_Number) {
+    #Desktop, All in One, treat as a desktop
+    3 { $Chassis_Type = "Desktop" }
+    13 { $Chassis_Type = "Desktop" }
+    #Laptop, Notebook, treat as a laptop
+    10 { $Chassis_Type = "Laptop" }
+    9 { $Chassis_Type = "Laptop" }
+}
+
+#Write out this custom property to the registry. This helps us with jobs and conditional branching in PDQ Deploy
+#Especially with GroupPolicies and SecurityPolicies
+New-ItemProperty -Path "HKLM:\HARDWARE\DESCRIPTION\System" -Name "ChassisType" -PropertyType "String" -Value $Chassis_Type -Force
